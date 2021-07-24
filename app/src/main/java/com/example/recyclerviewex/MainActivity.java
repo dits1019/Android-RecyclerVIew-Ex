ade843537a9f99e876890734e989ae97ea5303fd
dits1019 <jdits1019@gmail.com>
2021-07-24 17:58:40 +0900

    데이터소스와 레트로핏의 결합

--- app/src/main/java/com/example/recyclerviewex/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewex/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.recyclerviewex;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.paging.PageKeyedDataSource;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,9 +17,17 @@ import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
-
+    private PokeAPI pokeAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         recyclerView.addItemDecoration(new DivideDecoration(this));
+
+        // 레트로핏 초기화 과정
+        // Retrofit 빌더에 주소 중 변하지 않는 앞 부분을 넣어줌
+        // addConverterFactory에서 어떤 컨버터를 사용할지 지정하는 부분(현재는 Gson을 사용)
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        pokeAPI = retrofit.create(PokeAPI.class);
+
+        // 동기 인터페이스
+        try {
+            pokeAPI.listPokemons().execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -107,6 +132,68 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < parent.getChildCount(); i++){
                 final View view = parent.getChildAt(i);
                 c.drawLine(view.getLeft(), view.getBottom(), view.getRight(), view.getBottom(), paint);
+            }
+        }
+    }
+
+    private class DataSource extends PageKeyedDataSource<String, Result> {
+
+        // loadInitial 메서드를 구현해 처음으로 호출할 부분을 처리
+        // params를 활용하여 loadInitial 메서드 내에서 자료를 얻은 후 그 자료와 키들을 callback, onResult로 전달하는 식
+        // 데이터소스는 백그라운드에서 수행되기 때문에 별도의 비동기 인터페이스를 사용할 필요 X
+        @Override
+        public void loadInitial(@NonNull LoadInitialParams<String> params, @NonNull LoadInitialCallback<String, Result> callback) {
+            try {
+                // 동기식 인터페이스
+                Response body = pokeAPI.listPokemons().execute().body();
+                callback.onResult(body.results, body.previous, body.next);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 기존의 pokeAPI.listPokemons()로 이전 페이지와 다음 페이지를 구현할 수 없기 때문애 레트로핏 인터페이스를 확장
+
+        // 이전 페이지
+        @Override
+        public void loadBefore(@NonNull LoadParams<String> params, @NonNull LoadCallback<String, Result> callback) {
+            // pervious와 next 키가 주소?offset=숫자&limit=숫자 형태로 전달되어 오기 때문에 ?를 기준으로 잘라
+            // ("\\?) 쿼리 부분만 얻음
+            // "\\?"의 형태로 전달하는 것은 split 메서드가 ?를 와일드 카드로 해석해서 아무 문자열을 받아들이기 때문에
+            // \?를 전달하는 것이고, 문자열 내에서 \를 표현하기 위해 \를 두 번 사용
+            // 다시 "&"을 기준으로 쿼리를 하나하나 잘라내며, 다음으로 "="을 기준으로 잘라 키 밸류를 분리해서 사용
+            // loadBefore와 loadAfter는 유사한데 before는 params로 이전 키(previous)를 받고 콜백에도 새 이전 키를 반환
+            // after는 다음 키(next)를 받고 콜백에도 새 다음 키를 반환
+            String queryPart = params.key.split("\\?")[1];
+            String[] queries = queryPart.split("&");
+            Map<String, String> map = new HashMap<>();
+            for (String query : queries) {
+                String[] splited = query.split("=");
+                map.put(splited[0], splited[1]);
+            }
+            try {
+                Response body = pokeAPI.listPokemons(map.get("offset"), map.get("limit")).execute().body();
+                callback.onResult(body.results, body.previous);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 다음 페이지
+        @Override
+        public void loadAfter(@NonNull LoadParams<String> params, @NonNull LoadCallback<String, Result> callback) {
+            String queryPart = params.key.split("\\?")[1];
+            String[] queries = queryPart.split("&");
+            Map<String, String> map = new HashMap<>();
+            for (String query : queries) {
+                String[] splited = query.split("=");
+                map.put(splited[0], splited[1]);
+            }
+            try {
+                Response body = pokeAPI.listPokemons(map.get("offset"), map.get("limit")).execute().body();
+                callback.onResult(body.results, body.next);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
